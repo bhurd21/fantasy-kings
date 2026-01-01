392fbaec50c4068cbb42f2ad1f301004ca6f5722
Brennan Hurd <brennan@Brennans-MacBook-Air.local>
2026-01-01 16:33:22 -0700

    update for new year error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,30 +22,28 @@ class ApplicationController < ActionController::Base
   
   def current_nfl_week
     current_time = Time.current.in_time_zone('Eastern Time (US & Canada)')
-    year = current_time.year
-    
+
     # Weeks will change on Tuesday at 3 AM EST
-    season_start = Time.zone.parse("#{year}-09-02 03:00:00")
-    
+    season_start = Time.zone.parse("2025-09-02 03:00:00")
+
     hours_since_start = ((current_time - season_start) / 1.hour).floor
     days_since_start = (hours_since_start / 24.0).floor
     week = (days_since_start / 7.0).floor + 1
-    [[week, 1].max, 24].min
+    [[week, 1].max, 28].min
   end
   
   def nfl_week_for_date(date)
     return nil unless date
-    
+
     date_time = date.in_time_zone('Eastern Time (US & Canada)')
-    year = date_time.year
-    
+
     # Weeks will change on Tuesday at 3 AM EST
-    season_start = Time.zone.parse("#{year}-09-02 03:00:00")
-    
+    season_start = Time.zone.parse("2025-09-02 03:00:00")
+
     hours_since_start = ((date_time - season_start) / 1.hour).floor
     days_since_start = (hours_since_start / 24.0).floor
     week = (days_since_start / 7.0).floor + 1
-    [[week, 1].max, 24].min
+    [[week, 1].max, 28].min
   end
   
   def same_nfl_week?(date)
